61d36da71d8318e166493f80da755116eb16004d
Ahmed-Abdelhamid0 <Ahmed-Abdelhamid0@github.com>
2023-03-03 23:04:00 +0200

    first commit

---
+++ SWE2 Project/V6/HOSPITAL MANAGEMENT SYSTEM/src/hospital/management/system/HOSPITALMANAGEMENTSYSTEM.java
@@ -0,0 +1,13 @@
+package hospital.management.system;
+
+public class HOSPITALMANAGEMENTSYSTEM {
+
+    
+    public static void main(String[] args) {
+        Login_Form lf = new Login_Form();
+        lf.setVisible(true);
+        lf.setResizable(false);
+        
+    }
+    
+}
---
+++ V6/HOSPITAL MANAGEMENT SYSTEM/src/DBConector/HMS.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Mr.Antivirus
+ * Created: Dec 12, 2019
+ */
+
